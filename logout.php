6b6e1cbe18688c0b739f358270e7649c09ec4492
Olep8h <panov@student.vspj.cz>
2022-12-05 23:30:06 +0200

    logout

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+ # Logout script
+
+ 
+session_start();
+$_SESSION = array();
+session_destroy();
+if ($_SESSION["user_is_logged"])
+	echo "FATAL ERROR: Cannot terminate session!";
+else
+	header("Location: index.php");
+?>  
